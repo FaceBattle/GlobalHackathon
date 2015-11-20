8da63605da390162573e0c2f0d5b8daf0e3ebe0f
Luiz Fernando Cirigliano Villela <lf.villela@gmail.com>
2015-11-19 17:20:26 -0800

    adding index functions

--- GlobalHackGitHub.py
+++ GlobalHackGitHub.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/bias/', methods=['POST'])
+def find_bias():
+    word_searched = request.form['word']
+    print(word_searched)
+    return render_template('page.html', word=word_searched)
 
 
 if __name__ == '__main__':
