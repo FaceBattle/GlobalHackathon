76c62b13e300776c00155af0211f6b2d0b0d4967
Luiz Fernando Cirigliano Villela <lf.villela@gmail.com>
2015-11-19 17:24:44 -0800

    index template change

--- GlobalHackGitHub.py
+++ GlobalHackGitHub.py
@@ -3,7 +3,6 @@ from flask import render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -11,7 +10,7 @@ def hello_world():
 @app.route('/bias/', methods=['POST'])
 def find_bias():
     word_searched = request.form['word']
-    print(word_searched)
+
     return render_template('page.html', word=word_searched)
 
 
