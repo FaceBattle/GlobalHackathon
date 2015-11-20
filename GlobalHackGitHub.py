da12f30fab526092bb6a35b1f0511f1d280acc11
Daniel Pazinato <danielpazinato@Daniels-MacBook-Pro.local>
2015-11-20 00:47:00 -0800

    added normalization

--- GlobalHackGitHub.py
+++ GlobalHackGitHub.py
@@ -14,7 +14,7 @@ def find_bias():
     my_q = MyQuery()
 
     freq_dict = my_q.get_number_of_posts_for_word(word_searched)
-    pos_dict = my_q.main_word_every_page(word_searched)
+    pos_dict, mydict_main_words = my_q.main_word_every_page(word_searched)
 
 
     print(pos_dict)
