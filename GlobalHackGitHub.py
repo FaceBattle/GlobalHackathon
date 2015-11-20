f8efe2647e9db2a339fb6de36bca7d53b6049c69
Luiz Fernando Cirigliano Villela <lf.villela@gmail.com>
2015-11-19 21:43:26 -0800

    updating bubbles

--- GlobalHackGitHub.py
+++ GlobalHackGitHub.py
@@ -10,8 +10,26 @@ def hello_world():
 @app.route('/bias/', methods=['POST'])
 def find_bias():
     word_searched = request.form['word']
+    root_orig = {
+         "name": "pages",
+         "children": [
+             {"name": "aljazeera", "size": 3938},
+             {"name": "barackobama", "size": 743},
+             {"name": "bbcnews", "size": 24593},
+             {"name": "berniesanders", "size": 16540},
+             {"name": "CarlyFiorina", "size": 7074},
+             {"name": "cnn", "size": 1302},
+             {"name": "dawndotcom", "size": 24593},
+             {"name": "DonaldTrump", "size": 1302},
+             {"name": "FoxNews", "size": 1302},
+             {"name": "hillaryclinton", "size": 24593},
+             {"name": "nytimes", "size": 1302},
+             {"name": "sydneymorningherald", "size": 4896},
+             {"name": "TMZ", "size": 763}
+         ]
+    };
 
-    return render_template('page.html', word=word_searched)
+    return render_template('page.html', word=word_searched, bubble_data=root_orig)
 
 
 if __name__ == '__main__':
---
+++ static/js/bubbles.js
@@ -0,0 +1,73 @@
+/**
+ * Created by luizfernando2 on 11/19/15.
+ */
+
+function classes(root) {
+  var classes = [];
+
+  function recurse(name, node) {
+    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+    else classes.push({packageName: name, className: node.name, value: node.size, blabla: node.text});
+  }
+
+  recurse(null, root);
+  return {children: classes};
+}
+
+function make_bubbles(root, imagesFolder) {
+
+    var diameter = 480,
+            format = d3.format(",d"),
+            color = d3.scale.category20c();
+
+    var bubble = d3.layout.pack()
+            .sort(null)
+            .size([diameter, diameter])
+            .padding(1.5);
+
+    var svg = d3.select("#bubbleDiv").append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .style("display", "block")
+            .style("margin", "auto")
+            .attr("class", "bubble");
+
+
+    var node = svg.selectAll(".node")
+            .data(bubble.nodes(classes(root))
+                    .filter(function (d) {
+                        return !d.children;
+                    }))
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+    node.append("circle")
+            .attr("r", function (d) {
+                return d.r;
+            })
+            .style("fill", function (d) {
+                return color(d.packageName);
+            });
+    node.append('image')
+            .attr('xlink:href', function (d, i) {
+                return imagesFolder + d.className + '.png';
+            })
+            .attr('x', function (d, i) {
+                return -d.r / 2;
+            })
+            .attr('y', function (d, i) {
+                return -d.r / 2;
+            })
+            .attr('width', function (d, i) {
+                return d.r + 'px';
+            })
+            .attr('height', function (d, i) {
+                return d.r + 'px';
+            });
+
+
+    d3.select("#bubbleDiv").style("height", diameter + "px");
+}
